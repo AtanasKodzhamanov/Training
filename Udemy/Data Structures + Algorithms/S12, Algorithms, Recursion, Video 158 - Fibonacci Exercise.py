6a5b54e68731b80dd03511a82544eaded8d012d0
kodzh <39922895+kodzh@users.noreply.github.com>
2022-07-04 03:17:07 +0100

    Udemy

--- Udemy/Data Structures + Algorithms/S12, Algorithms, Recursion, Video 158 - Fibonacci Exercise.py
+++ Udemy/Data Structures + Algorithms/S12, Algorithms, Recursion, Video 158 - Fibonacci Exercise.py
@@ -1,12 +1,12 @@
-def Recursive(n):
+def Recursive(n): #O(2^N) exponential time
     if n<2: return n
     return Recursive(n-1)+Recursive(n-2)
 
-def Iterative(n):
+def Iterative(n): #O(n)
     seq=[0,1]
     for i in range(1,n+1):
         seq.append(seq[i]+seq[i-1])
     return seq[n]
 
-print(Recursive(10))
-print(Iterative(10))
+print(Recursive(42))
+print(Iterative(69))
