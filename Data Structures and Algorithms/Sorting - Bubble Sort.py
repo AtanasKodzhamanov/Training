e3bc0acc01f7ae8706c6823086a9e64f0a56d5eb
kodzh <39922895+kodzh@users.noreply.github.com>
2022-07-09 16:14:36 +0100

    Sorting

--- Data Structures and Algorithms/Sorting - Bubble Sort.py
+++ Data Structures and Algorithms/Sorting - Bubble Sort.py
@@ -1,8 +1,8 @@
 
-unsorted=[100, 40, 60, 10, 0, -40, -200, -30]
+unsorted=[100, 40, 60, 10, 0, -40, -200, -30, 1, 2, 3, 4, 5, 6, -5]
 
 def BubbleSort(list):
-    for _ in range(len(list)-1):
+    for i in range(0,len(list)):
         for j in range(0,len(list)-1):
             if list[j]>list[j+1]:
                 temp=list[j]
