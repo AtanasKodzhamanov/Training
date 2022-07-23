39eea0d211707218b1bcf85d784147cd991cab70
kodzh <39922895+kodzh@users.noreply.github.com>
2022-07-23 04:15:00 +0100

    SoftUnit - JS Advanced

--- Software University (SoftUni)/JavaScript Advanced/04. JS-Advanced-DOM-Introduction-Exercise-Resources/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/04. Search in List/search.js
+++ Software University (SoftUni)/JavaScript Advanced/04. JS-Advanced-DOM-Introduction-Exercise-Resources/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/04. Search in List/search.js
@@ -1,3 +1,19 @@
 function search() {
-   // TODO
+   let liElements = document.getElementsByTagName("li")
+   let inputText = document.getElementById("searchText").value
+   let count = 0
+
+   for (let element of liElements) {
+
+      element.style.fontWeight = ""
+      element.style.textDecoration = ""
+
+      if (element.textContent.includes(inputText)) {
+         element.style.fontWeight = "bold"
+         element.style.textDecoration = "underline"
+         count++
+      }
+
+   }
+   document.getElementById("result").textContent = `${count} matches found`
 }
