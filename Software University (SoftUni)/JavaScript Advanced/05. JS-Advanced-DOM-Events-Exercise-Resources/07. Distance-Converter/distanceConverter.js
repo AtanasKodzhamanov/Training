50c2347b41676108964ef4cb635a6590621325bc
kodzh <39922895+kodzh@users.noreply.github.com>
2022-07-24 01:19:40 +0100

    JS

--- Software University (SoftUni)/JavaScript Advanced/05. JS-Advanced-DOM-Events-Exercise-Resources/07. Distance-Converter/distanceConverter.js
+++ Software University (SoftUni)/JavaScript Advanced/05. JS-Advanced-DOM-Events-Exercise-Resources/07. Distance-Converter/distanceConverter.js
@@ -1,3 +1,19 @@
 function attachEventsListeners() {
-    console.log('TODO:...');
+    let [inputField, outputField]=document.querySelectorAll('input[type="text"]')
+    let inputUnits=document.querySelector('#inputUnits')
+    let outUnits=document.querySelector('#outputUnits')
+
+    let buttonEl=document.getElementById("convert")
+
+    buttonEl.addEventListener("click",onClick)
+
+    function onClick(event){
+        let convert = Number(inputField.value)
+        let result=0
+
+        //conversion calc
+
+    }
+
+
 }
