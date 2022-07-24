4440a9c0223fc022651437c3fb65eea9130079d8
kodzh <39922895+kodzh@users.noreply.github.com>
2022-07-24 01:03:32 +0100

    JS

--- Software University (SoftUni)/JavaScript Advanced/05. JS-Advanced-DOM-Events-Exercise-Resources/02. Time-Converter/solution.js
+++ Software University (SoftUni)/JavaScript Advanced/05. JS-Advanced-DOM-Events-Exercise-Resources/02. Time-Converter/solution.js
@@ -1,4 +1,22 @@
 function attachEventsListeners() {
 
-    console.log('TODO:...');
+    let ratios={
+        days:1,
+        hours:24,
+        minutes:1440,
+        seconds:86400
+    }
+
+    document.getElementById("daysBtn").addEventListener("click",onConvert)
+    document.getElementById("hoursBtn").addEventListener("click",onConvert)
+    document.getElementById("minutesBtn").addEventListener("click",onConvert)
+    document.getElementById("secondsBtn").addEventListener("click",onConvert)
+
+    function onConvert(event){
+        let input= event.target.parentElement.querySelector('input[type="text"]')
+        let time = convert (Number(input.value), input.id)
+
+        //add calcs
+        
+    }
 }
