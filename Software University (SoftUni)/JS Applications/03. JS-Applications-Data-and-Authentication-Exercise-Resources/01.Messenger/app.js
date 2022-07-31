ad2462ed9f3ecad2bb0d2dd77ae5e6ca9c5e948e
kodzh <39922895+kodzh@users.noreply.github.com>
2022-07-31 18:35:02 +0100

    JS

--- Software University (SoftUni)/JS Applications/03. JS-Applications-Data-and-Authentication-Exercise-Resources/01.Messenger/app.js
+++ Software University (SoftUni)/JS Applications/03. JS-Applications-Data-and-Authentication-Exercise-Resources/01.Messenger/app.js
@@ -1,5 +1,46 @@
+const url =`http://localhost:3030/jsonstore/messenger`
+const messages = document.getElementById("messages")
+
 function attachEvents() {
-    console.log('TODO...');
+    document.getElementById("submit").addEventListener("click", postMessage)
+    document.getElementById("refresh").addEventListener("click", loadAllMessages)
+}
+
+async function postMessage(){
+    const[author,content]=[document.getElementById("author"),document.getElementById("content")]
+    if(author.value !== "" || content.value!==""){
+        await request(url,{author:author.value, content:content.value})
+        author.value=""
+        content.value=""
+    }
+    
+}
+
+async function loadAllMessages(){
+    const res = await fetch(url)
+    const data = await res.json()
+    console.log(res)
+    console.log(data)
+
+    messages.value=Object.values(data).map(({author, content})=>
+        `${author}: ${content}`).join("\n")
+    
+}
+
+async function request(url, option){
+    if(option){
+    
+        option = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json;charset=utf-8"
+        },
+        body: JSON.stringify(option)
+        }
+    }
+    const response = await fetch(url,option)
+
+    return response.json()
 }
 
 attachEvents();
