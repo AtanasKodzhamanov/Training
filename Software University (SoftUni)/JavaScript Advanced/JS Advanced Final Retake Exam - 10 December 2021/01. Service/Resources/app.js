2a22e84f37f60a16a46fc49a9af064706fbb1f0e
kodzh <39922895+kodzh@users.noreply.github.com>
2022-08-08 03:42:00 +0100

    JS

--- Software University (SoftUni)/JavaScript Advanced/JS Advanced Final Retake Exam - 10 December 2021/01. Service/Resources/app.js
+++ Software University (SoftUni)/JavaScript Advanced/JS Advanced Final Retake Exam - 10 December 2021/01. Service/Resources/app.js
@@ -1,5 +1,21 @@
 window.addEventListener('load', solve);
 
 function solve() {
-    //TODO
+    //Description, client name, and client phone are non-empty strings. If any of them are empty, the program should not do anything. 
+
+    //get input elements
+    const productTypeEl=document.getElementById("type-product")
+    const descriptionEl=document.getElementById("description")
+    const clientNameEl=document.getElementById("client-name")
+    const phoneEl=document.getElementById("client-phone")
+
+    //get other forms
+
+    //get buttons
+    submitBtn=document.querySelector("#right > form > button")
+    
+    submitBtn.addEventListener("click",(e)=>{
+        console.log(submitBtn)
+    })
+
 }
