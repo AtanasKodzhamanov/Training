e2f7d85d0f0091215c34d17eab6726ebd36a105b
kodzh <39922895+kodzh@users.noreply.github.com>
2022-08-10 07:07:27 +0100

    JS

---
+++ Software University (SoftUni)/JavaScript Advanced/JS Advanced Final Exam - 10 Aug 2022/03. Movie Theater/MovieTheater/test.js
@@ -0,0 +1,36 @@
+let {expect} = require("chai")
+let companyAdministration=require("./companyAdministration")
+
+describe("nameoffiletobetested",()=>{
+    describe("funcname", ()=>{
+	    it("test 1",()=>{
+            expect(()=>nameoffiletobetested.funcname()).to.throw() 
+        })
+
+        it("test 1",()=>{
+            expect(()=>nameoffiletobetested.funcname()).to.throw() 
+        })
+    })
+
+
+    describe("funcname", ()=>{
+        it("test 1",()=>{
+            expect(()=>nameoffiletobetested.funcname()).to.throw() 
+        })
+
+        it("test 1",()=>{
+            expect(()=>nameoffiletobetested.funcname()).to.throw() 
+        })        
+    })
+
+
+    describe("funcname", ()=>{
+        it("test 1",()=>{
+            expect(()=>nameoffiletobetested.funcname()).to.throw() 
+        })
+        
+        it("test 1",()=>{
+            expect(()=>nameoffiletobetested.funcname()).to.throw() 
+        })        
+    })
+})
