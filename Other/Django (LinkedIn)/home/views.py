d8708dd99eb488287bd296d58941e766227be54d
kodzh <39922895+kodzh@users.noreply.github.com>
2022-08-29 18:31:26 +0100

    .

--- Other/Django (LinkedIn)/home/views.py
+++ Other/Django (LinkedIn)/home/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime 
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def home(request):
+    #return HttpResponse("Hellow, world")
+    return render(request, "home/welcome.html", {"today":datetime.today})
+
+@login_required(login_url="/admin")
+def authorised(request):
+    return render(request, "home/authorised.html", {})
