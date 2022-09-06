6c11b20002b09079a5fcedc49f5aea29a84cd9a6
kodzh <39922895+kodzh@users.noreply.github.com>
2022-09-06 01:46:07 +0100

    Leetcode

--- Data Structures and Algorithms/LeetCode/414. Third Maximum Number.py
+++ Data Structures and Algorithms/LeetCode/414. Third Maximum Number.py
@@ -21,3 +21,15 @@ class Solution:
         if second == initial or third== initial:
             return first
         return third
+
+
+# Neater
+
+    def thirdMax(self, nums):
+        v = [float('-inf'), float('-inf'), float('-inf')]
+        for num in nums:
+            if num not in v:
+                if num > v[0]:   v = [num, v[0], v[1]]
+                elif num > v[1]: v = [v[0], num, v[1]]
+                elif num > v[2]: v = [v[0], v[1], num]
+        return max(nums) if float('-inf') in v else v[2]
