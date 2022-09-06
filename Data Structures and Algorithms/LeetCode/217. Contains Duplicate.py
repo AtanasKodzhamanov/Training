4b0b10deee4069a9cdceaf81f66c0bebd37e7c48
kodzh <39922895+kodzh@users.noreply.github.com>
2022-09-06 23:46:22 +0100

    Leet

---
+++ Data Structures and Algorithms/LeetCode/217. Contains Duplicate.py
@@ -0,0 +1,6 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        setNums=set(nums)
+        if len(setNums)<len(nums):
+            return True
+        return False
