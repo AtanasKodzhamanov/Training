684bc76401153b3148dacfea34f13b1da7962edc
kodzh <39922895+kodzh@users.noreply.github.com>
2022-09-08 02:58:44 +0100

    DJ

---
+++ Software University (SoftUni)/Python Web Basics/Workshop/ProjectFolder/petstagram/main/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+from django.core.validators import MinLengthValidator
+
+from petstagram.petstagram.main.validators import only_letters_validator
+
+# Create your models here.
+class Profile(models.Models):
+    FIRST_NAME_MIN_LENGTH=2
+    LAST_NAME_MIN_LENGTH=2    
+    FIRST_NAME_MAX_LENGTH=30
+    LAST_NAME_MAX_LENGTH=30
+
+    MALE="Male"
+    FEMALE="Female"
+    DO_NOT_SHOW="Do not show"
+
+    GENDERS=[(x,x) for x in (MALE,FEMALE,DO_NOT_SHOW)]
+
+
+    #id/pk by default
+    first_name=models.CharField(
+        max_length=FIRST_NAME_MAX_LENGTH,
+        validators=(
+            MinLengthValidator(FIRST_NAME_MIN_LENGTH),
+            only_letters_validator
+        )
+    )
+    first_name=models.CharField(
+        max_length=LAST_NAME_MAX_LENGTH,
+        validators=(
+            MinLengthValidator(LAST_NAME_MIN_LENGTH),
+            only_letters_validator
+        )
+    )
+    picture=models.URLField()
+
+    date_of_birth=models.DateField(
+        null=True,
+        blank=True
+    )
+    description=models.TextField(
+        null=True,
+        blank=True,
+    )
+    gender=models.CharField(
+        max_length=max(len(x) for x, _ in GENDERS),
+        choices=GENDERS,
+        null=True,
+        blank=True,
+    )
+
