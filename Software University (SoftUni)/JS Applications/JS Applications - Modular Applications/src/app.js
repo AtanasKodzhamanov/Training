3819e4c2f635236eb3b39d322526a4a55fd3fda9
Atanas <kodzhamanov@gmail.com>
2023-01-16 17:57:04 +0000

    Upload packages

---
+++ Software University (SoftUni)/JS Applications/JS Applications - Modular Applications/src/app.js
@@ -0,0 +1,8 @@
+import page from '../node_modules/page/page.mjs';
+import { homeView } from './views/homeView.js';
+import { loginView } from './views/loginView.js';
+
+page("/", homeView);
+page("/login", loginView);
+
+page.start();
