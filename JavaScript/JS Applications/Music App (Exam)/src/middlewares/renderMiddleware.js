b6e84666fcd1633d3757410acfd9ab33016d90ea
Atanas <kodzhamanov@gmail.com>
2023-04-11 05:54:32 +0100

    JS apps games play

--- JavaScript/JS Applications/Music App (Exam)/src/middlewares/renderMiddleware.js
+++ JavaScript/JS Applications/Music App (Exam)/src/middlewares/renderMiddleware.js
@@ -1,11 +1,21 @@
-import {render} from "../../node_modules/lit-html/lit-html.js"
+import { render } from "../../node_modules/lit-html/lit-html.js"
 import { navigationView } from "../views/navigationView.js";
 
 const headerElement = document.querySelector(".header-navigation")
+const mainElement = document.querySelector("#main-content")
 
-export const renderNavigation = (ctx, next) =>{
+const renderContent = (templateResult) => {
+    render(templateResult, contentElement);
+}
+
+export const renderNavigationMiddleware = (ctx, next) => {
     //Render navigation 
-    
+
     render(navigationView(ctx), headerElement);
+    next()
+}
+
+export const renderContentMiddleware = (ctx, next) => {
+    ctx.render = (content) => render(content, mainElement);
     next()
 }
